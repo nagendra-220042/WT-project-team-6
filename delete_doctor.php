ee474b69b7d73ae586d5f2e6d9d9f1fc085d36ee
jamuna-220382 <n220382@rguktn.ac.in>
2026-03-25 16:20:59 +0530

    Update delete_doctor.php crctly

--- delete_doctor.php
+++ delete_doctor.php
@@ -6,10 +6,9 @@ $did = $_POST['did'];
 $sql = "DELETE FROM Doctors WHERE did='$did'";
 
 if($conn->query($sql)){
-// header("Location: admin_login.php");
     echo "Deleted Successfully";
 
 }else{
-echo "Error: ".$conn->error;
+echo "Error : ".$conn->error;
 }
 ?>
